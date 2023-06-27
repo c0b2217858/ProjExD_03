8a0cc1c530a079bc2eaa00ce19ee18b27d7a6117
c0b2217858 <c0b2217858@edu.teu.ac.jp>
2023-06-27 16:40:39 +0900

    kansei

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -38,19 +38,19 @@ class Bird:
     def __init__(self, num: int, xy: tuple[int, int]):
         """
         こうかとん画像Surfaceを生成する
-        引数1 num：こうかとん画像ファイル名の番号
-        引数2 xy：こうかとん画像の位置座標タプル
+        引数1 num:こうかとん画像ファイル名の番号
+        引数2 xy:こうかとん画像の位置座標タプル
         """
         img0 = pg.transform.rotozoom(pg.image.load(f"ex03/fig/{num}.png"), 0, 2.0)  # 左向き
         img = pg.transform.flip(img0, True, False)  # 右向き
         self.imgs = {
-            (+5, 0): img,  # 右
+            (+5,  0): img,  # 右
             (+5, -5): pg.transform.rotozoom(img, 45, 1.0),  # 右上
-            (0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
+            ( 0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
             (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
-            (-5, 0): img0,  # 左
+            (-5,  0): img0,  # 左
             (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
-            (0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
+            ( 0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
             (+5, +5): pg.transform.rotozoom(img, -45, 1.0),  # 右下
         }
         self.img = self.imgs[(+5, 0)]  # デフォルト：右向き
